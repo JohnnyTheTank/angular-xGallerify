6505b5fe550e7d4e36b07bb13fa5a9ba4156cb9c
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-03-21 15:22:50 +0100

    impr: removed unneeded vars

--- dist/angular-xgallerify.js
+++ dist/angular-xgallerify.js
@@ -1,42 +1,35 @@
 /**
     @name: angular-xgallerify 
-    @version: 1.0.2 (21-01-2016) 
+    @version: 1.0.2 (21-03-2016) 
     @author: Jonathan Hornung <jonathan.hornung@gmail.com> 
     @url: https://github.com/JohnnyTheTank/angular-xGallerify#readme 
     @license: MIT
 */
-var jjtXGallerify = angular.module('jtt_angular_xgallerify', []);
-
-jjtXGallerify.directive('xgallerify', function () {
-    return {
-        restrict: 'A',
-        replace: 'false',
-        link: function (scope, element, attrs) {
-
-            var settings = {};
-            var params = {};
-
-            if(attrs.xgallerify) {
-                params = $.parseJSON(attrs.xgallerify.replace(/'/g, '"'));
-            }
-
-            if (isObject(params)) {
-                settings = $.extend({
-                    margin:5,
-                    mode:'bootstrap',
-                    lastRow:'adjust'
-                }, params );
-            }
-
-            element.gallerify(settings);
-
-            scope.$on('angular-xGallerify.refresh', function() {
-                element.gallerify.render();
-            });
-
-            function isObject(item) {
-                return (typeof item === "object" && !Array.isArray(item) && item !== null);
+angular.module('jtt_angular_xgallerify', [])
+    .directive('xgallerify', function () {
+        return {
+            restrict: 'A',
+            replace: 'false',
+            link: function (scope, element, attrs) {
+                var settings = {};
+                var params = {};
+                if (attrs.xgallerify) {
+                    params = $.parseJSON(attrs.xgallerify.replace(/'/g, '"'));
+                }
+                if (isObject(params)) {
+                    settings = $.extend({
+                        margin: 5,
+                        mode: 'bootstrap',
+                        lastRow: 'adjust'
+                    }, params);
+                }
+                element.gallerify(settings);
+                scope.$on('angular-xGallerify.refresh', function () {
+                    element.gallerify.render();
+                });
+                function isObject(item) {
+                    return (typeof item === "object" && !Array.isArray(item) && item !== null);
+                }
             }
         }
-    }
-});
+    });
