a0a6e7efc3354f5a904e4a261851d05ded38d72d
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-03-21 15:21:00 +0100

    impr: removed unneeded vars

--- src/angular-xgallerify.js
+++ src/angular-xgallerify.js
@@ -1,35 +1,28 @@
-var jjtXGallerify = angular.module('jtt_angular_xgallerify', []);
-
-jjtXGallerify.directive('xgallerify', function () {
-    return {
-        restrict: 'A',
-        replace: 'false',
-        link: function (scope, element, attrs) {
-
-            var settings = {};
-            var params = {};
-
-            if(attrs.xgallerify) {
-                params = $.parseJSON(attrs.xgallerify.replace(/'/g, '"'));
-            }
-
-            if (isObject(params)) {
-                settings = $.extend({
-                    margin:5,
-                    mode:'bootstrap',
-                    lastRow:'adjust'
-                }, params );
-            }
-
-            element.gallerify(settings);
-
-            scope.$on('angular-xGallerify.refresh', function() {
-                element.gallerify.render();
-            });
-
-            function isObject(item) {
-                return (typeof item === "object" && !Array.isArray(item) && item !== null);
+angular.module('jtt_angular_xgallerify', [])
+    .directive('xgallerify', function () {
+        return {
+            restrict: 'A',
+            replace: 'false',
+            link: function (scope, element, attrs) {
+                var settings = {};
+                var params = {};
+                if (attrs.xgallerify) {
+                    params = $.parseJSON(attrs.xgallerify.replace(/'/g, '"'));
+                }
+                if (isObject(params)) {
+                    settings = $.extend({
+                        margin: 5,
+                        mode: 'bootstrap',
+                        lastRow: 'adjust'
+                    }, params);
+                }
+                element.gallerify(settings);
+                scope.$on('angular-xGallerify.refresh', function () {
+                    element.gallerify.render();
+                });
+                function isObject(item) {
+                    return (typeof item === "object" && !Array.isArray(item) && item !== null);
+                }
             }
         }
-    }
-});
+    });
